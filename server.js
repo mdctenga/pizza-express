262fde756e53fb9c1a9d56159eda6304282343b6
mdctenga <mdctenga@gmail.com>
2015-07-15 19:18:10 -1000

    Set up postgres database

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+var config = require('./config/config.json');
+
+// GET method route
+app.get('/', function (req, res) {
+  res.send('GET request to the homepage');
+});
+
+// POST method route
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage');
+});
+
+var server = app.listen(config.port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+// var sequelize = new Sequelize('database', 'username', 'password', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+
+// });
+
+// var sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+
+// var User = sequelize.define('user', {
+//   firstName: {
+//     type: Sequelize.STRING,
+//     field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+//   },
+//   lastName: {
+//     type: Sequelize.STRING
+//   }
+// }, {
+//   freezeTableName: true // Model tableName will be the same as the model name
+// });
+
+// User.sync({force: true}).then(function () {
+//   // Table created
+//   return User.create({
+//     firstName: 'John',
+//     lastName: 'Hancock'
+//   });
+// });
